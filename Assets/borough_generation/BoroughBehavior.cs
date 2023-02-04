6ac92ee3b16cb43d69f5e02fc5902a3333c5b2d0
Basil42 <32388487+Basil42@users.noreply.github.com>
2023-02-04 16:33:51 +0100

    merging borough placer

--- Assets/borough_generation/BoroughBehavior.cs
+++ Assets/borough_generation/BoroughBehavior.cs
@@ -1,9 +1,28 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
 
 namespace borough_generation
 {
     public class BoroughBehavior : MonoBehaviour
     {
         public Node Tile;
+        public List<MeshRenderer> elements;
+
+        private void Start()
+        {
+            RaycastHit hit;
+            for (int i = 0; i < elements.Count; i++)
+            {
+                var elementTrans = elements[i].transform;
+
+                if (Physics.Raycast(elementTrans.position, Vector3.down, out hit))//TODO: multiple ray cast to average the normal so "cliff houses" don't fuck up too hard
+                {
+                    elementTrans.position = hit.point + new Vector3(0f,elements[i].bounds.extents.y,0f);//not ideal but okay
+                    elementTrans.rotation = Quaternion.FromToRotation(Vector3.up, hit.normal);
+                }
+                
+            }
+        }
     }
 }
