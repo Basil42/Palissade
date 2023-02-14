ea8b353227cf8c12c04d769c08e84b3c4112e35c
Basil42 <32388487+Basil42@users.noreply.github.com>
2023-02-14 17:07:55 +0100

    snapped placed castle to terrain

--- Assets/Scripts/Objects/CastleBehavior.cs
+++ Assets/Scripts/Objects/CastleBehavior.cs
@@ -1,18 +1,25 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
+[ExecuteAlways]
 public class CastleBehavior : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private RaycastHit _hit;
+    private Transform _transRef;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        #if UNITY_EDITOR
+        if (_transRef == null) _transRef = transform;
+        if (transform.hasChanged && Physics.Raycast(new Vector3(_transRef.position.x,40f,_transRef.position.z),Vector3.down,out _hit))
+        {
+            var position = _transRef.position;
+            _transRef.position = new Vector3(position.x, _hit.point.y, position.z);
+        }
+        #endif
     }
+    
 }
